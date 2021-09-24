034c70ee52e9477b19c0e7072790e296ee37e57b
sxu <laozishixs@gmail.com>
2021-09-24 20:38:50 +0200

    Fix bug in titles and add single/multiple file mode

--- main.py
+++ main.py
@@ -4,14 +4,15 @@
 #####################
 
 import requests
+import csv
 
 baseUrl = 'https://www.hikrobotics.com'
 summaryListUrl = baseUrl + '/en/Api/Foreground/Vision/VisionProductContent?'
 detailUrl = baseUrl + '/en/Api/Foreground/Vision/VisionProductConfig?'
 
 
-def getSummaryData(): # 1 file, 29 lines
-    paramsSummary = {'firstModuleId': 78, 'page': 1, 'secondaryModuleId': 42, 'size': 100, 'screening': None}
+def getSummaryData(firstModuleId): # 1 file, 29 lines
+    paramsSummary = {'firstModuleId': firstModuleId, 'page': 1, 'secondaryModuleId': 42, 'size': 100, 'screening': None}
     resp = requests.get(summaryListUrl, paramsSummary)
     data = resp.json()
     visionProductConfig = data['data']['VisionProductConfig'] # list, len = 29
@@ -21,18 +22,18 @@ def getSummaryData(): # 1 file, 29 lines
     dataList = []
     for productConfig in visionProductConfig:
         dataDict = {}
-        for i in range(len(productConfig)):
+        for i in range(len(titleList)):
             if i < 2:
                 dataDict[titleList[i]] = visionProductContent[i][titleList[i]]
             else:
                 dataDict[titleList[i]] = productConfig[i - 2]
         dataList.append(dataDict)
     print('Now exporting summary(table) data as CSV file')
-    exportObjectAsCSV(titleList, dataList)
+    exportObjectAsCSV(titleList, dataList, 'Table', True)
     return visionProductContent
 
 
-def getDetailedData(productSummary): # 29 files, each file has 33 lines -> each dict creates a file
+def getDetailedData(productSummary): # 29 files, each file has 33 columns -> each dict creates a file
     titleList = []
     dataList = []
     for productIdx, product in enumerate(productSummary):
@@ -41,33 +42,38 @@ def getDetailedData(productSummary): # 29 files, each file has 33 lines -> each 
         paramsDetail = {'id': product['id']}
         respData = requests.get(detailUrl, paramsDetail).json()['data']
         if len(respData) > 0:
-            titleList = list(respData[0].keys())
             for respLine in respData:
+                if productIdx == 0:
+                    titleList.append(respLine['name'])
                 dataDict[respLine['name']] = respLine['value']
             dataList.append(dataDict)
     print('Now exporting detailed data as CSV file')
-    exportObjectAsCSV(titleList, dataList)
+    exportObjectAsCSV(titleList, dataList, 'Detail')
     print('Successfully fetched all Hikrobot data!')
 
 
 
-
-def parseResponseAsJson():
-    pass
-
-
-def exportObjectAsCSV(titles, dataList):
+def exportObjectAsCSV(titles, dataList, filePattern, onlyOneFile=False):
     """
     :param titles: List
     :param data:  List -> a dataList can have multiple dicts. Each dict generates a file
     """
     print(titles)
-    for index, data in enumerate(dataList):
-        # To-Do: instead of printing, generate a CSV file for each dict in the list
-        print('Line', index + 1, ':', data)
+    if onlyOneFile:
+        with open('c:\\hikdata\\' + filePattern + '.csv', mode='w') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(titles)
+            for index, data in enumerate(dataList):
+                csv_writer.writerow(list(data.values()))
+    else:
+        for index, data in enumerate(dataList):
+            with open('c:\\hikdata\\' + str(index) + '-' + filePattern + '.csv', mode='w') as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(titles)
+                csv_writer.writerow(list(data.values()))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    productSummary = getSummaryData()
-    getDetailedData(productSummary)
+    productSummary = getSummaryData(78)
+    # getDetailedData(productSummary)
